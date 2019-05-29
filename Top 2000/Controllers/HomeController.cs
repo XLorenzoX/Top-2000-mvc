d14ee4a6b8d9d0f2d0143350faee5d737865533b
Lorenzo Peperkamp <0257180@student.rocvantwente.nl>
2019-05-29 14:20:11 +0200

    Add project files.

---
+++ Top 2000/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+using System.Web.Mvc;
+
+using Top_2000.Models;
+
+namespace Top_2000.Controllers
+
+{
+
+    public class HomeController : Controller
+
+    {
+
+      
+
+        public ActionResult Index()
+
+        {
+
+            return View();
+
+        }
+
+    }
+
+}
---
+++ Top 2000/Models/LijstsController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Top_2000.Models
+{
+    public class LijstsController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Lijsts
+        public ActionResult Index()
+        {
+            var lijst = db.Lijst.Include(l => l.Song).Where(x => x.top2000jaar == 2018);
+            return View(lijst.OrderBy(x => x.positie).ToList());
+        }
+
+        // GET: Lijsts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lijst lijst = db.Lijst.Find(id);
+            if (lijst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lijst);
+        }
+
+        // GET: Lijsts/Create
+        public ActionResult Create()
+        {
+            ViewBag.songid = new SelectList(db.Song, "songid", "titel");
+            return View();
+        }
+
+        // POST: Lijsts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "songid,top2000jaar,positie")] Lijst lijst)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lijst.Add(lijst);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.songid = new SelectList(db.Song, "songid", "titel", lijst.songid);
+            return View(lijst);
+        }
+
+        // GET: Lijsts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lijst lijst = db.Lijst.Find(id);
+            if (lijst == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.songid = new SelectList(db.Song, "songid", "titel", lijst.songid);
+            return View(lijst);
+        }
+
+        // POST: Lijsts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "songid,top2000jaar,positie")] Lijst lijst)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lijst).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.songid = new SelectList(db.Song, "songid", "titel", lijst.songid);
+            return View(lijst);
+        }
+
+        // GET: Lijsts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lijst lijst = db.Lijst.Find(id);
+            if (lijst == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lijst);
+        }
+
+        // POST: Lijsts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lijst lijst = db.Lijst.Find(id);
+            db.Lijst.Remove(lijst);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
